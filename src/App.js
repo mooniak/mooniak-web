12934160f5862cdd35cab0cad7f5569d11fd7b14
Azela <asela@leafycode.com>
2017-07-05 14:05:45 +0530

    added styled compoinents

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Component/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
       </div>
     );
   }
